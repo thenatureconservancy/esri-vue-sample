422a643b4767e560a34095095b49cf733e54e96e
Matt Silveira <msilveira@tnc.org>
2021-08-10 13:15:04 -0600

    lazy load community planner

--- src/router/index.js
+++ src/router/index.js
@@ -21,7 +21,10 @@ const routes = [
   {
     path: "/community-planner",
     name: "CommunityPlanner",
-    component: CommunityPlanner,
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import("../components/CommunityPlanner.vue"),
   },
 ];
 
